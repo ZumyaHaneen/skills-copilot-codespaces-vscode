fded49fbd7862c316792dd28f86eca540d22f999
Muniba <78030816+ZumyaHaneen@users.noreply.github.com>
2024-02-21 15:03:33 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,35 +1,34 @@
-//create web server
+// create web server
+// create a web server that listens to port 3000
+// when a request is made to the server, the server will return a list of comments in JSON format
+// the server will also handle POST requests to add new comments
+
 var http = require('http');
-var fs = require('fs');
-var path = require('path');
 var url = require('url');
-var comments = [];
-var server = http.createServer(function(req, res){
-    var urlObj = url.parse(req.url, true);
-    var pathname = urlObj.pathname;
-    if(pathname == '/'){
-        fs.readFile('./index.html', function(err, data){
-            res.writeHead(200, {'Content-Type':'text/html'});
-            res.end(data);
-        });
-    }else if(pathname == '/submit'){
-        var comment = urlObj.query;
-        comments.push(comment);
-        res.end(JSON.stringify(comments));
-    }else if(pathname == '/getComments'){
-        var json = JSON.stringify(comments);
-        res.end(json);
-    }else{
-        fs.exists('.'+pathname, function(exists){
-            if(exists){
-                fs.readFile('.'+pathname, function(err, data){
-                    res.end(data);
-                });
-            }else{
-                res.statusCode = 404;
-                res.end('not found');
-            }
-        });
+var items = [];
+
+var server = http.createServer(function(req, res) {
+    switch (req.method) {
+        case 'POST':
+            var item = '';
+            req.setEncoding('utf8');
+            req.on('data', function(chunk) {
+                item += chunk;
+            });
+            req.on('end', function() {
+                items.push(item);
+                res.end('OK\n');
+            });
+            break;
+        case 'GET':
+            var body = items.map(function(item, i) {
+                return i + ') ' + item;
+            }).join('\n');
+            res.setHeader('Content-Length', Buffer.byteLength(body));
+            res.setHeader('Content-Type', 'text/plain; charset="utf-8"');
+            res.end(body);
+            break;
     }
 });
-server.listen(8080);
+
+server.listen(3000);
